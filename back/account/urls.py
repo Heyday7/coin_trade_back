9d0ab942476c6cca9cea76807b467dd09afac1a2
Heyday7 <kth3447@gmail.com>
2021-05-19 15:20:11 +0900

    view와 url 작성

---
+++ back/account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers, urlpatterns
+from .views import *
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'account', AccountViewSet)
+
+urlpatterns = [
+  path('', include(router.urls)),
+  path('login', LoginView.as_view(), name='login'),
+  path('logout', LogoutView.as_view(), name='logout'),
+]
--- back/account/views.py
+++ back/account/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import logout
+from .serializers import AccountSerializer, LoginSerializer
 from django.shortcuts import render
+from rest_framework import views, viewsets
+from rest_framework.response import Response
+from .models import Account
 
-# Create your views here.
+
+class AccountViewSet(viewsets.ModelViewSet):
+	queryset = Account.objects.all()
+	serializer_class = AccountSerializer
+
+
+class LoginView(views.APIView):
+	def post(self, request, *args, **kwargs):
+		serializer = LoginSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		serializer.perform_login(request)
+		return Response(serializer.data)
+
+class LogoutView(views.APIView):
+	def post(self, request, *args, **kwargs):
+		logout(request)
+		return Response()
+
