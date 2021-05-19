b0ec7890117d9818bb5d42981bd5e337d26897c6
Heyday7 <kth3447@gmail.com>
2021-05-19 15:18:25 +0900

    account model 생성

--- back/account/models.py
+++ back/account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Account(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	balance = models.PositiveBigIntegerField(default=0)
 
 # Create your models here.
