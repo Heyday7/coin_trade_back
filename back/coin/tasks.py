07dbe30d819c44cec43180fca667970280b8251f
Heyday7 <kth3447@gmail.com>
2021-05-20 10:12:08 +0900

    주기적 task에서 coin 객체를 생성하도록 설정

--- back/coin/tasks.py
+++ back/coin/tasks.py
@@ -1,9 +1,16 @@
 from __future__ import absolute_import, unicode_literals 
 from celery import shared_task 
+from .serializers import CoinSerializer
+from .models import Coin
 import requests
 
 @shared_task
 def get_coin_data(coin):
-  url = f'https://api.bithumb.com/public/ticker/{coin}_KRW'
-  response = requests.get(url)
-  return response.json()
+    url = f'https://api.bithumb.com/public/ticker/{coin}_KRW'
+    response = requests.get(url)
+    data = response.json()
+    data['data']['name'] = coin
+
+    if data['status'] == '0000':
+        Coin.objects.create(**data['data'])
+        
