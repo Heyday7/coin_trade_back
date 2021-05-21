614ef920f5d290205509b588184384bdbb1dc964
Heyday7 <kth3447@gmail.com>
2021-05-21 16:30:17 +0900

    TransactionView 작성
    모델링의 가장 핵심적인 부분이다.
    코인의 가격도 여기서 받아오고,
    transaction과 coinholding 두가지의 serializer를 작동시켜
    결과물로 transaction과 coinholding object를 둘다 만들거나 수정한다.
    자세한 내용은 따로 블로그 글로 적어둔다

--- back/account/views.py
+++ back/account/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth import logout
-from .serializers import AccountSerializer, LoginSerializer
-from django.shortcuts import render
+from .serializers import AccountSerializer, LoginSerializer, TransactionSerializer
+from django.shortcuts import get_object_or_404
 from rest_framework import views, viewsets
 from rest_framework.response import Response
 from .models import Account
+from coin.utils import get_coin_price
+from coin.models import Coin, CoinHolding
+from coin.serializers import CoinHoldingSerializer
 
 
 class AccountViewSet(viewsets.ModelViewSet):
@@ -18,8 +21,34 @@ class LoginView(views.APIView):
 		serializer.perform_login(request)
 		return Response(serializer.data)
 
+
 class LogoutView(views.APIView):
 	def post(self, request, *args, **kwargs):
 		logout(request)
 		return Response()
 
+
+class TransactionView(views.APIView):
+    def post(self, request, *args, **kwargs):
+        price = get_coin_price(request.data['ticker'])['data']['closing_price']
+        coin, create = Coin.objects.get_or_create(ticker=request.data['ticker'])
+        data = {**request.data, 
+            "price":price, 
+            "coin":coin.id,
+            "account" : Account.objects.get(user=self.request.user).id,
+            "total_price": float(price) * request.data['coin_amount']
+        }
+        tran_seri = TransactionSerializer(data=data)
+
+        try:
+            instance = get_object_or_404(CoinHolding, account=data['account'], coin=data['coin'])
+            coin_hold_seri = CoinHoldingSerializer(instance=instance, data=data)
+        except:
+            coin_hold_seri = CoinHoldingSerializer(data=data)
+
+        tran_seri.is_valid(raise_exception=True)
+        coin_hold_seri.is_valid(raise_exception=True)
+        tran_seri.save()
+        coin_hold_seri.save()
+
+        return Response(tran_seri.data)
