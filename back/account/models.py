d4f2db7d58b17310a0618467f623939000b1710c
Heyday7 <kth3447@gmail.com>
2021-05-21 16:23:50 +0900

    Transaction 모델 작성 및 account.balance 기본 1억으로 설정

--- back/account/models.py
+++ back/account/models.py
@@ -4,6 +4,19 @@ from django.contrib.auth.models import User
 
 class Account(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	balance = models.PositiveBigIntegerField(default=0)
+	balance = models.PositiveBigIntegerField(default=100000000)
 
-# Create your models here.
+
+class Transaction(models.Model):
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE,
+        related_name='transactions',
+        related_query_name='transaction'
+    )
+    type = models.CharField(max_length=10, default='buy')
+    coin = models.ForeignKey('coin.Coin', on_delete=models.CASCADE)
+    price = models.FloatField()
+    total_price = models.FloatField()
+    coin_amount = models.FloatField()
+
