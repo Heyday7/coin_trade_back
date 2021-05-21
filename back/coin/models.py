536ed8b486be6223e2bdd7199fb1713bae4cdf5c
Heyday7 <kth3447@gmail.com>
2021-05-21 16:24:43 +0900

    coin 관련 모델 수정
    기존의 Coin이었던 coin 가격 데이터 모델을 CoinPrice로 변경
    새로운 Coin 모델 작성, Coin의 이름과 ticker를 나타내는 일반적인 정보를 담는 모델이다.
    CoinHolding 모델 작성 : 각 계정에 코인이 얼마나 담겨있는지를 나타내기 위한 모델
    	price : 매수단가
    	total_price : 매수에 사용한 총 금액
    	coin : 보유중인 코인 종류
    	coin_amount : 보유중인 코인의 양

--- back/coin/models.py
+++ back/coin/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+
 class Coin(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=100, null=True)
+    ticker = models.CharField(max_length=10)
+    description = models.TextField(default=None, null=True)
+
+    def __str__(self):
+        return self.ticker
+
+
+class CoinPrice(models.Model):
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
     opening_price = models.FloatField()
     closing_price = models.FloatField()
     min_price = models.FloatField()
@@ -17,3 +27,14 @@ class Coin(models.Model):
     fluctate_rate_24H = models.FloatField()
     date = models.DateTimeField(auto_now=True)
     
+
+class CoinHolding(models.Model):
+    account = models.ForeignKey(
+        'account.Account', 
+        on_delete=models.CASCADE,
+        related_name='coinholdings',
+        related_query_name='coinholding')
+    price = models.FloatField()
+    total_price = models.FloatField()
+    coin_amount = models.FloatField()
+    coin = models.ForeignKey(Coin, on_delete=models.CASCADE)
